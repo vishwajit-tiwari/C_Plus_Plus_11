2ad05e9d37289c7ce8ca35811b5ccb60dc36cab8
vishwajit-tiwari <vishwajit16897@gmail.com>
2025-01-23 00:05:13 +0530

    auto with iterators, vector and initializer_list

--- auto/autoWithIterators/autoWithIterators.cpp
+++ auto/autoWithIterators/autoWithIterators.cpp
@@ -1,5 +1,5 @@
 /**
- * @brief auto with iterators
+ * @brief auto with iterators, vector and initializer_list
  * @author Vishwajit Tiwari
  * @date 22-01-2025
  */
@@ -12,6 +12,7 @@ using namespace std;
 
 int main() {
 
+#ifdef VECTOR
     vector<int> numbers = {1, 2, 3, 4, 5};
 
     for (auto iterator = numbers.begin(); iterator != numbers.end(); iterator++) 
@@ -22,5 +23,21 @@ int main() {
             cout<<endl;
         }
     }
+
+#else
+
+    initializer_list<int> num {1, 2, 3, 4, 5};
+
+    for (auto iterator = num.begin(); iterator != num.end(); iterator++) 
+    {
+        cout<< *iterator<< " ";
+        if(iterator == (num.end()-1))
+        {
+            cout<<endl;
+        }
+    }
+
+#endif
+
     return 0;
 }
